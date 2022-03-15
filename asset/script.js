37f76b0f7d9c7fcaad4e7e335057772e8d4cc455
Nicolas <nicolaslibert.pro@gmail.com>
2022-03-15 19:35:43 +0100

    Ajout de fonctionnalités

--- asset/script.js
+++ asset/script.js
@@ -11,26 +11,89 @@ let canonWidth = 50;
 let canonHeight = 150;
 let canonX = (canvas.width / 2) - canonWidth;
 let canonY = (canvas.height) - canonHeight;
-c.fillStyle = 'rgb(255,69,0)';
-c.fillRect(canonX, canonY, canonWidth, canonHeight);
-console.log(canonX);  
+
+// Arc, Circles
+let cibleX;
+let cibleY;
+target();
+
+// Projectil
+let projectilX = 0;
+let projectilY = 0;
+
+// Counter
+let counter = 0;
+
+run();
+
+function target()
+{
+    cibleX = Math.random() * canvas.width;
+    cibleY = Math.random() * 200;
+}
 
 window.addEventListener('keyup', (e) => {
     switch (e.key) {
         case 'ArrowLeft':
-           canonX = canonX - 5;
+           canonX = canonX - 25;
             break;
         case 'ArrowRight':
-           canonX = canonX + 5;
+           canonX = canonX + 25;
+            break;
+        case ' ':
+            projectilX = canonX + (canonWidth / 2);
+            projectilY = canonY;
             break;
     }
 });
 
-// Arc, Circles
-c.beginPath();
-c.arc(150, 100, 35, 0, Math.PI * 2, false);
-c.strokeStyle = 'blue';
-c.stroke();
+
+function updateScreen()
+{
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    // Mise à jour du déplacement du canon
+    c.fillStyle = 'rgb(255,69,0)';
+    c.fillRect(canonX, canonY, canonWidth, canonHeight);
+
+    // Mise à jour de la cible
+    c.beginPath();
+    c.arc(cibleX, cibleY, 35, 0, Math.PI * 2, false);
+    c.strokeStyle = 'blue';
+    c.stroke(); 
+
+    // Mise à jour du projectile
+    c.beginPath();
+    c.arc(projectilX, projectilY, 15, 0, Math.PI * 2, false);
+    c.strokeStyle = 'red';
+    c.stroke();
+    projectilY-= 7;
+
+    // Mise à jour de la collision
+    var dx = projectilX - cibleX;
+    var dy = projectilY - cibleY;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < 15 + 35) {        
+            target();
+            projectilX = 0;
+            projectilY = 0;
+            counter++;
+    }
+
+    // Mise à jour du counter
+    c.fillStyle = 'grey';
+    c.font = "30px Arial";
+    c.fillText(counter, 10, 50);    
+}
+
+// On ne sort pas des functions qui sont "prises" dans un callback tant qu'on ne le précise pas
+// Les valeurs de base ne sont donc pas reprise
+function run() {
+    updateScreen();
+      window.requestAnimationFrame(run);
+}
+
+
 
 // Lignes
 // c.beginPath();
